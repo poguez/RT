3e0da8bf8229a1900d52b6cd101d3d65c8beee3b
Miguel Palau <mpalauzarza@gmail.com>
2018-03-18 17:43:32 -0600

    scrollSpy situation done

--- src/assets/js/navigation/navigation.js
+++ src/assets/js/navigation/navigation.js
@@ -1,11 +1,13 @@
 import stickyNavigation from './sticky-navigation'
 import headroom from './headroom'
 import scrollToTop from './scroll-to-top'
+import scrollSpy from './scroll-spy'
 
 const navigation = () => {
   headroom().init()
   stickyNavigation()
   scrollToTop()
+  scrollSpy()
 }
 
 export default navigation
--- src/assets/js/navigation/scroll-spy.js
+++ src/assets/js/navigation/scroll-spy.js
@@ -1,1 +1,8 @@
-// gumshoe
+import gumshoe from 'gumshoejs'
+import { wrapper } from './wrapper'
+
+const scrollSpy = () => gumshoe.init({
+  offset: wrapper.offsetHeight
+})
+
+export default scrollSpy
