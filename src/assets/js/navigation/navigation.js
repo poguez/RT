5c7fd6552588b4b757844fdec708515f37819c34
Miguel Palau <mpalauzarza@gmail.com>
2018-03-18 17:47:10 -0600

    navigation js situation done

--- src/assets/js/navigation/navigation.js
+++ src/assets/js/navigation/navigation.js
@@ -2,12 +2,14 @@ import stickyNavigation from './sticky-navigation'
 import headroom from './headroom'
 import scrollToTop from './scroll-to-top'
 import scrollSpy from './scroll-spy'
+import smoothScroll from './smooth-scroll'
 
 const navigation = () => {
   headroom().init()
   stickyNavigation()
   scrollToTop()
   scrollSpy()
+  smoothScroll()
 }
 
 export default navigation
