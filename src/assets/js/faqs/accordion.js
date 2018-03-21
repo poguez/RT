316213d35147ffff647c507a9079ac5cbcee9eeb
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-03-21 16:41:02 -0600

    bootlag accordion crustacean situation :fish:

--- src/assets/js/faqs/accordion.js
+++ src/assets/js/faqs/accordion.js
@@ -1,3 +1,25 @@
-const accordion = () => {}
+import loopQuery from '../tools/loopQuery'
+import { faqs } from 'styles'
+
+const accordion = () => {
+  const accordions = document.querySelectorAll('[data-accordion]')
+  const { accordion: styles } = faqs
+
+  loopQuery(accordions, accordion => {
+    const content = accordion.querySelector('[data-content]')
+    content.style.maxHeight = '0'
+    content.style.overflow = 'hidden'
+    content.style.transition = '0.25s'
+
+    const button = accordion.querySelector('[data-trigger]')
+    button.addEventListener('click', event => {
+      accordion.classList.toggle(styles.active)
+
+      content.style.maxHeight = accordion.classList.contains(styles.active)
+        ? `${content.scrollHeight}px`
+        : '0'
+    })
+  })
+}
 
 export default accordion
