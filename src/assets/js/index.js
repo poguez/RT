e3ddfb926e9a91e7dcc63973553041516d868e30
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-03-21 14:35:51 -0600

    adding header styles desk

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,5 @@
 // General Code for all pages
 import asyncFonts from './tools/async-fonts'
-import faqs from './faqs'
 
 asyncFonts({
   href: 'https://fonts.googleapis.com/css?family=Lato:700,900|Source+Sans+Pro:400,400i,600,700,900',
@@ -20,4 +19,9 @@ if (document.querySelector('#hero')) {
   })
 }
 
-faqs()
+// Homepage
+if (document.querySelector('#faqs-page')) {
+  import(/* webpackChunkName: "faqs" */ './faqs').then(faqs => {
+    faqs.default()
+  })
+}
