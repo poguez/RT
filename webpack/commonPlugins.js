46901488cdeb5ad0691c4c7c3d8fac79c34335cb
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-03-22 09:21:05 -0600

    tweaked the webpack warning a bit

--- webpack/commonPlugins.js
+++ webpack/commonPlugins.js
@@ -24,9 +24,10 @@ let common = [
 *  ###  ###  ##     ## ##     ## ##    ## #### ##    ##  ######   ####
 *
 *  Don't edit this file directly. Edit with Webpack.
+*  Then after you're done run npm run build
 *
 *  Scaffolded with @pixel2html/generator-frontend
-**********************************************************************/
+*  ********************************************************************/
 `,
     raw: true
   })
