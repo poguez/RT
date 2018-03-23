df49e5330c28daf81227355fc2cca3846862e734
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-03-23 01:12:50 -0600

    we have to return to the slider situation later on

--- src/assets/js/home/home.js
+++ src/assets/js/home/home.js
@@ -2,12 +2,14 @@ import navigation from './navigation'
 import video from './video';
 import team from './team'
 import roadmap from './roadmap'
+import problem from './problem'
 
 const home = () => {
   navigation()
   video()
   team()
   roadmap()
+  problem()
 }
 
 export default home
---
+++ src/assets/js/home/problem.js
@@ -0,0 +1,9 @@
+import { tns as tinySlider } from 'tiny-slider/src/tiny-slider.module'
+import { home } from 'styles'
+
+const problem = () =>
+  tinySlider({
+    container: home.problem.problems
+  })
+
+export default problem
